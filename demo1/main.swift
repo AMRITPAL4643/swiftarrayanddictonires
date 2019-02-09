761627a6e8d21e7567d3573f2175ef91eb378456
MacStudent <macstudent@126-imac14.local>
2019-02-08 19:37:08 -0500

    various declaration of array and array with slices

--- demo1/main.swift
+++ demo1/main.swift
@@ -49,3 +49,79 @@ if(y.contains(-1000))
     print("true")
 }
 
+
+//array declaration
+var a = [10,20,30,40,50]
+print(a.count)
+for i in a
+{
+    print(i)
+}
+print(a[0], a[1], a[3], a[4])
+//print(a[5])
+//a[5] = 1000
+//print(a[5])
+var b = [Int]()
+b.append(100)
+b.append(200)
+b.append(300)
+print(b[0], b[1], b[2])
+
+b += [111,222,333]
+print(b)
+print("values of c array")
+let c = a + b
+//for i in c
+//{
+  //  print(i)
+//}
+//slice style of array
+print("values of c[1..3] array")
+var m =  c[1...3]
+    print(m)
+        m[1] = 2020
+for i in x
+{
+    print(i)
+}
+print("slice values of c[1] array")
+print(c[1])
+var n = c[3...10]
+print(n[3])
+
+//another  declaration of array
+var z = Array<Int>()
+z.append(1000)
+print(z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
