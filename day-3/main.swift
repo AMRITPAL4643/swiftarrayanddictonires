87c6cf72a7d21fdb440e9c58cedb3c2deebf6b89
MacStudent <macstudent@126-iMac07.local>
2019-02-11 18:58:13 -0500

    function declarations

--- day-3/main.swift
+++ day-3/main.swift
@@ -9,4 +9,35 @@
 import Foundation
 
 print("Hello, World!")
+// function declaration
+func sum(a: Int, b: Int)
+{
+    let c = a + b;
+    print("sum:\(c)")
+}
+sum(a: 10, b: 20)
+
+func sum( a: Float, b : Float)
+{
+    let c = a + b
+    print("sum - F : \(c)")
+}
+sum(a: 10.5, b: 10.5)
+
+
+//labels in function
+//func sum(of a: Float, b : Float)
+//{
+  //  let c = a + b
+    //print("sum - F : \(c)")
+//}
+//sum(of: 10.5, and: 10.5)
+
+func sub( _ a: Int, _ b : Int)
+{
+    let c = a - b
+    print("sub : \(c)")
+}
+sub(200,6)
+
 
