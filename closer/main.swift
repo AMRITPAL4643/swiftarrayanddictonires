c6cc8e567307ea8baef45b73986987afe0b2769a
macos <macos@macoss-Mac.local>
2019-02-19 16:05:35 -0800

    closer

--- closer/main.swift
+++ closer/main.swift
@@ -8,5 +8,32 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Hello, World")
+
+// closer
+
+ let names = ["amrit","alex","barry","ewa","addfg"]
+print(names)
+func backward(_ s1: String, _ s2: String) -> Bool
+{
+return s1 < s2
+}
+func forward(_ s1: String, _ s2: String) -> Bool
+{
+  return s1 > s2
+}
+var reverseNames1 = names.sorted(by: backward)
+print(reverseNames1)
+var reverseNames2 = names.sorted(by: forward)
+print(reverseNames2)
+var reverseNames3 = names.sorted {( a: String, b:String) -> Bool in
+
+return a > b
+}
+print(reverseNames3)
+var reverseNames4 = names.sorted {( a , b) -> Bool in
+  return a > b
+}
+print(reverseNames4)
+
 
